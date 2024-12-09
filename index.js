c1430310684025fe74b8e60c052d84ba36cdff8d
Adarsh A Devadiga <adarsh9742007@gmail.com>
2024-12-10 00:40:36 +0530

    latest

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import ejs from "ejs";
+import {dirname} from 'path';
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 4000;
+const __dirname = dirname(fileURLToPath(import.meta.url));  
+
+app.set('view engine','ejs');
+app.set('views', `${__dirname}/views`);
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.listen(`${port}`, ()=>{
+    console.log(`Server running on port ${port}`);
+})
